dd2ee723b888000382795e2fb0595f6c906c1b0f
elmeddin <elmeddin@globalsoft.az>
2022-02-02 11:04:59 +0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>  
+  <div class="container">
+    <h1 class="text-center">Restaurants</h1>
+    <div class="row">
+      <div class="col-sm-12 col-md-6 col-lg-4 col-xl-3">
+        <div class="card">
+          <img src="https://source.unsplash.com/RVF0ngUujks/1200x900" alt="" class="card-img-top">
+          <h4 class="card-title">Lorem, ipsum.</h4>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+
+</style>
